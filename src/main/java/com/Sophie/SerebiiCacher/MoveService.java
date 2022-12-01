725abc465ec26f3785e82163a3ee38dcf2535519
Sophie Lavigne <wylthhalm@gmail.com>
2022-11-30 20:08:54 -0500

    Starting the project.

---
+++ src/main/java/com/Sophie/SerebiiCacher/MoveService.java
@@ -0,0 +1,5 @@
+package com.Sophie.SerebiiCacher;
+
+public class MoveService {
+
+}
---
+++ src/test/java/com/Sophie/SerebiiCacher/SerebiiCacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sophie.SerebiiCacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerebiiCacherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
